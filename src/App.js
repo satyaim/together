2fc81fa66fc09e7bfa0853f6510682d715d01495
Satyavrat Sharma <thisissatyavrat@gmail.com>
2021-06-06 04:52:07 +0530

    Add React Player

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactPlayer from 'react-player/lazy'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {videoFilePath: "", canPlay: false};
+  }
+
+  handleVideoUpload = (event) => {
+    console.log( URL.createObjectURL(event.target.files[0]), ReactPlayer.canPlay(URL.createObjectURL(event.target.files[0])))
+    this.setState({canPlay: ReactPlayer.canPlay(URL.createObjectURL(event.target.files[0]))})
+    this.setState({videoFilePath: URL.createObjectURL(event.target.files[0])});
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="file" onChange={this.handleVideoUpload} />
+        <ReactPlayer url={this.state.videoFilePath} width="100%" height="100%" controls={true} onError={e => console.log('onError', e)} />
+      </div>
+    );
+  }
 }
 
 export default App;
