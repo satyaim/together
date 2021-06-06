f2605a987b6d5bf55638819972c174a9dc7d6012
Satyavrat Sharma <thisissatyavrat@gmail.com>
2021-06-06 23:30:56 +0530

    Add feathers client

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactPlayer from 'react-player/lazy'
+import client from './feathers';
+import Login from './login';
 
 class App extends React.Component {
 
@@ -8,6 +10,16 @@ class App extends React.Component {
     this.state = {videoFilePath: "", canPlay: false};
   }
 
+  componentDidMount() {
+    console.log("did mount()")
+    // Try to authenticate with the JWT stored in localStorage
+    client.authenticate().catch(() => this.setState({ login: null }));
+    // On successfull login
+    client.on('authenticated', login => {
+      console.log("authenticated", login)
+      this.setState({ login });
+    });
+  }
   handleVideoUpload = (event) => {
     console.log( URL.createObjectURL(event.target.files[0]), ReactPlayer.canPlay(URL.createObjectURL(event.target.files[0])))
     this.setState({canPlay: ReactPlayer.canPlay(URL.createObjectURL(event.target.files[0]))})
@@ -15,12 +27,13 @@ class App extends React.Component {
   };
 
   render() {
-    return (
-      <div>
+    if(this.state.login) {
+      return <div>
         <input type="file" onChange={this.handleVideoUpload} />
         <ReactPlayer url={this.state.videoFilePath} width="100%" height="100%" controls={true} onError={e => console.log('onError', e)} />
       </div>
-    );
+    }
+    return <Login />;
   }
 }
 
